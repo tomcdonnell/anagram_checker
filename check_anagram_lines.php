03f50ccddaa8e465c0b07e4f0015a4ff48cf3a72
Tom McDonnell <tomcdonnell@gmail.com>
2012-04-14 13:54:13 +1000

    Moved directories up one level after removing 'trunk'.

---
+++ check_anagram_lines.php
@@ -0,0 +1,200 @@
+<?php
+/**************************************************************************************************\
+*
+* vim: ts=3 sw=3 et wrap co=100 go -=b
+*
+* Filename: "check_anagram_lines.php"
+*
+* Project: Anagram Poem Checker.
+*
+* Purpose: Check that each line of a given text consists only of anagrams of a given string.
+*
+* Author: Tom McDonnell 2010-11-16.
+*
+\**************************************************************************************************/
+
+// Includes. ///////////////////////////////////////////////////////////////////////////////////////
+
+require_once dirname(__FILE__) . '/../library/tom/php/utils/Utils_html.php';
+
+// Settings. ///////////////////////////////////////////////////////////////////////////////////////
+
+error_reporting(-1);
+session_start();
+
+// Globally executed code. /////////////////////////////////////////////////////////////////////////
+
+try
+{
+   if (!array_key_exists('anagramPhrase', $_POST) || !array_key_exists('textToCheck', $_POST))
+   {
+      throw new Exception('This page has been used incorrectly.');
+   }
+
+   if (strlen($_POST['textToCheck']) > 500000)
+   {
+      throw new Exception('Text to check exceeded length limit.');
+   }
+
+   $_SESSION['anagramChecker'] = $_POST;
+}
+catch (Exception $e)
+{
+   echo $e->getMessage();
+   die;
+}
+
+// Functions. //////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ *
+ */
+function stripNonAlphaCharactersFromString($string)
+{
+   $strlen    = strlen($string);
+   $newString = '';
+
+   for ($i = 0; $i < $strlen; ++$i)
+   {
+      $char = $string[$i];
+
+      if (ctype_alpha($char))
+      {
+         $newString .= $char;
+      }
+   }
+
+   return strtolower($newString);
+}
+
+/*
+ * String A is the master string against which string B is checked.
+ */
+function checkAnagram($stringA, $stringB)
+{
+   if (strlen($stringA) != strlen($stringB))
+   {
+      return false;
+   }
+
+   $missingLettersString = '';
+   $extraLettersString   = '';
+
+   for ($i = 0; $i < strlen($stringA); ++$i)
+   {
+      $char = $stringA[$i];
+      $posB = strpos($stringB, $char);
+
+      if ($posB === false)
+      {
+         $missingLettersString .= $char;
+      }
+      else
+      {
+         $stringB = substr_replace($stringB, '', $posB, 1);
+      }
+   }
+
+   $extraLettersString = $stringB;
+
+   if (strlen($extraLettersString) == 0)
+   {
+      return true;
+   }
+
+   return array($missingLettersString, $extraLettersString);
+}
+
+function echoMessage($color, $string)
+{
+   echo "<span style='color: ", Utils_html::escapeSingleQuotes($color), "'>";
+   echo htmlentities($string);
+   echo "</span><br/>\n";
+}
+
+// HTML code. //////////////////////////////////////////////////////////////////////////////////////
+?>
+<!DOCTYPE html PUBLIC
+ "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+ <head><title>Anagram Checker</title></head>
+ <body>
+  <a class='backLink' href='http://www.tomcdonnell.net'>Back to tomcdonnell.net</a> |
+  <a class='backLink' href='http://www.tomcdonnell.net/small_apps/anagram_finder'>Anagram Finder</a>
+  <h1>Anagram Checker</h1>
+  <p>Use this tool to check that all lines of a section of text are anagrams of a given phrase.</p>
+  <hr/>
+  <a href='index.php'>Back</a>
+  <p><?php echo $_POST['anagramPhrase']; ?> (&lt;-- Given Phrase)</p>
+  <p>
+<?php
+$linesToCheck                = explode("\n", trim($_POST['textToCheck']));
+$anagramPhrase               = trim($_POST['anagramPhrase']);
+$strippedAnagramPhrase       = stripNonAlphaCharactersFromString($anagramPhrase);
+$strlenStrippedAnagramPhrase = strlen($strippedAnagramPhrase);
+$nPerfectLines               = 0;
+
+foreach ($linesToCheck as $line)
+{
+   $line = str_replace("\r", '', $line);
+   echo "   $line";
+
+   $strippedLine       = stripNonAlphaCharactersFromString($line);
+   $strlenStrippedLine = strlen($strippedLine);
+
+   if ($strlenStrippedLine != $strlenStrippedAnagramPhrase)
+   {
+      $difference     = $strlenStrippedLine - $strlenStrippedAnagramPhrase;
+      $differenceWord = ($difference > 0)? 'many': 'few';
+      $differenceAbs  = abs($difference);
+      $lettersWord    = ($differenceAbs == 1)? 'letter': 'letters';
+      echoMessage('#f00', " ($differenceAbs $lettersWord too $differenceWord)");
+      continue;
+   }
+
+   $checkResult = checkAnagram($strippedAnagramPhrase, $strippedLine);
+
+   if (is_array($checkResult))
+   {
+      echoMessage('#f00', "(missing: '{$checkResult[0]}', extra: '{$checkResult[1]}')");
+   }
+   else
+   {
+      if ($checkResult === false)
+      {
+         throw new Exception('Unexpected result.');
+      }
+
+      echoMessage('#0f0', '(perfect anagram)');
+      ++$nPerfectLines;
+   }
+}
+?>
+  </p>
+<?php
+$nImperfectLines = count($linesToCheck) - $nPerfectLines;
+$stylesString    = 'width: 500px; padding: 10px; text-align: center;';
+
+if ($nImperfectLines == 0)
+{
+   $stylesString .= ' background: #00ff00;';
+   $message       = 'Perfect Result!';
+}
+else 
+{
+   $stylesString .= ' background: #ff0000;';
+   $message       = "$nImperfectLines incorrect lines were found.";;
+}
+?>
+  <p style='<?php echo Utils_html::escapeSingleQuotes($stylesString); ?>'>
+<?php
+echo '   ', htmlentities($message), "\n";
+?>
+  </p>
+  <a href='index.php'>Back</a>
+ </body>
+</html>
+<?php
+/*******************************************END*OF*FILE********************************************/
+?>
---
+++ index.php
@@ -0,0 +1,127 @@
+<?php
+/**************************************************************************************************\
+*
+* vim: ts=3 sw=3 et wrap co=100 go -=b
+*
+* Filename: "index.php"
+*
+* Project: Anagram Poem Checker.
+*
+* Purpose: The main file for the project.
+*
+* Author: Tom McDonnell 2010-11-16.
+*
+\**************************************************************************************************/
+
+// Settings. ///////////////////////////////////////////////////////////////////////////////////////
+
+error_reporting(-1);
+session_start();
+
+// Globally executed code. /////////////////////////////////////////////////////////////////////////
+
+try
+{
+   $action =
+   (
+      (array_key_exists('action', $_GET))? $_GET['action']:
+      (
+         (array_key_exists('anagramChecker', $_SESSION))? 'addSessionText': 'clearForm'
+      )
+   );
+
+   switch ($action)
+   {
+    case 'addExampleText':
+      $anagramPhrase = 'Only Anagram Lines';
+      $textToCheck   = <<<STR
+I yell anagrams. Non-
+angry ones. In a mall,
+I normally nag sane,
+loyal, Grannies. Man,
+orals agleam, ninny
+as neon! Alarmingly,
+any moral leanings
+annoy managers. Ill-
+mannerly as-in gaol
+mongrels. Nail any a
+mall-Grannie. Say 'No',
+an' earn manly slog. I
+annoy ill managers.
+Alas, any lemon-grin,
+lone salarying man
+yearns manna. Go, ill-
+ninny! Slam a lager! O,
+lager! Sin on layman,
+miner's agony. All an
+angler inlays. Moan
+insanely man, or lag
+solemnly. A ring, an' a
+gal. Loins yearn, man.
+A lone, grisly man, an'
+a mall. Syringe on an
+alloy sign. Amen. Ran
+lanes angrily. Am no
+ogre. Any ill man's an
+amoral ninny. A leg's
+mangy, sore. All in an
+alien slag. Ran on my
+leg, airman nylons a
+meanly sling. An oar!
+A nail! My angler son! -
+on a Granny? - I smell a
+mil ale! 'Son, a Granny!
+In son! Yell! Anagram!'
+STR;
+      break;
+    case 'addSessionText':
+      $anagramPhrase = $_SESSION['anagramChecker']['anagramPhrase'];
+      $textToCheck   = $_SESSION['anagramChecker']['textToCheck'  ];
+      break;
+    case 'clearForm':
+      $anagramPhrase = '';
+      $textToCheck   = '';
+      break;
+    default:
+      throw new Exception("Unknown action '$action'.");
+   }
+}
+catch (Exception $e)
+{
+   echo $e->getMessage();
+   die;
+}
+
+// HTML code. //////////////////////////////////////////////////////////////////////////////////////
+?>
+<!DOCTYPE html PUBLIC
+ "-//W3C//DTD XHTML 1.0 Strict//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+ <head><title>Anagram Checker</title></head>
+ <body>
+  <a class='backLink' href='http://www.tomcdonnell.net'>Back to tomcdonnell.net</a> |
+  <a class='backLink' href='http://www.tomcdonnell.net/small_apps/anagram_finder'>Anagram Finder</a>
+  <h1>Anagram Checker</h1>
+  <p>Use this tool to check that all lines of a given text are anagrams of a given phrase.</p>
+  <hr/>
+  <p>
+   <a href='index.php?action=addExampleText'>Add example text</a> |
+   <a href='index.php?action=clearForm'>Clear text</a>
+  </p>
+  <form action='view-source:check_anagram_lines.php' method='post'>
+   <p>
+    Anagram Phrase:<br/>
+    <input type='text' name='anagramPhrase' size='80' value='<?php echo $anagramPhrase; ?>'/>
+   </p>
+   <p>
+    Text to check:<br/>
+    <textarea rows='30' cols='80' name='textToCheck'><?php echo $textToCheck;?></textarea>
+   </p>
+   <input type='submit' value='Submit'/>
+  </form>
+ </body>
+</html>
+<?php
+/*******************************************END*OF*FILE********************************************/
+?>
